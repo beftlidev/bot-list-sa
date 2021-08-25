6f83fae5ae030a53f848f9e44edfa0121c6c62bc
iUgur <77486662+iUgur@users.noreply.github.com>
2021-08-25 08:18:20 +0300

    Update Ban.js

--- komutlar/Ban.js
+++ komutlar/Ban.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js')
-const db = require('quick.db')
+const db = require('croxydb')
 const ayarlar = require('../ayarlar.json')
 exports.run = async(client, message, args) => {
 const Embed = new Discord.MessageEmbed()
 .setColor('BLUE')
-.setTitle('⚠ Hata!')
 .setDescription('Bu komutu kullanabilmek için `'+message.guild.roles.cache.find(Roller => Roller.id === ayarlar.BANYetkilisi).name+'` rolüne sahip olman gerekli.')
 if (!message.member.roles.cache.find(Rol => Rol.id === ayarlar.BANYetkilisi)) return message.channel.send(Embed).then(Message => Message.delete({timeout: 7500}))
 const Pepe = message.mentions.users.first()
